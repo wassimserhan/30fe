cdbe859fb13d4cfe066e89aaae9b36e7d542b2fc
Wassim Serhan <wserhan@gmail.com>
2023-08-29 20:28:36 -0400

    edit

--- wp-content/themes/30fe/single.php
+++ wp-content/themes/30fe/single.php
@@ -17,6 +17,7 @@
             </section>
             <h1 class="single-post__headline"><?php the_title() ?></h1>
             <p class="single-post__copy"><?php echo $subtitle ?></p>
+            <p class="single-post__date"><?php echo get_the_date( 'F j, Y' ); ?></p>
           </article>
           <figure class="single-post__figure">
             <img loading="lazy" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo $alt; ?>"
