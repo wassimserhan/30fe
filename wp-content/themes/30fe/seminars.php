464af30f272a786964626b4b8120eb2cf1784b8f
Wassim Serhan <wserhan@gmail.com>
2023-08-30 15:18:43 -0400

    edit

---
+++ wp-content/themes/30fe/seminars.php
@@ -0,0 +1,30 @@
+<?php /* Template Name: About */ ?>
+
+<?php 
+get_header();
+?>
+
+
+<main class="main-container">
+  <section class="single-post">
+    <section class="single-blog fullscreen">
+      <section class="max-width max-padding">
+        <section class="single-blog__single-post">
+          <?php
+          while( have_posts()) {
+          the_post();
+          ?>
+          <!-- Display Content -->
+          <?php echo the_content();?>
+        </section>
+        <?php }  ?>
+
+      </section>
+    </section>
+  </section>
+
+</main>
+
+<?php 
+get_footer();
+?>
