8f7d685831c504f9c0e2649613e76a968f5a4351
Wassim Serhan <wserhan@gmail.com>
2023-09-04 10:08:35 -0400

    edit

--- wp-content/themes/30fe/single-sector.php
+++ wp-content/themes/30fe/single-sector.php
@@ -95,6 +95,16 @@ get_header();
   <!-- Expert Form -->
   <section class="sector-form fullscreen">
     <section class="sector-form__wrapper max-width max-padding">
+      <section class="sector-form__form">
+        <h3 class="sector-form__headline">For more Information</h3>
+        <p class="sector-form__form__copy">Fill up this form and someone from our team will be in touch with you
+          shortly.</p>
+        <p class="sector-form__form__required">*REQUIRED</p>
+        <!-- form inserted here -->
+        <!-- <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 2 ) ); ?> -->
+        <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 1 ) ); ?>
+        <!-- form inserted here -->
+      </section>
       <?php 
 
               $relatedSectorTeam = get_posts(array(
@@ -121,16 +131,7 @@ get_header();
       <?php endforeach; ?>
       <?php wp_reset_postdata() ?>
 
-      <section class="sector-form__form">
-        <h3 class="sector-form__headline">For more Information</h3>
-        <p class="sector-form__form__copy">Fill up this form and someone from our team will be in touch with you
-          shortly.</p>
-        <p class="sector-form__form__required">*REQUIRED</p>
-        <!-- form inserted here -->
-        <!-- <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 2 ) ); ?> -->
-        <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 1 ) ); ?>
-        <!-- form inserted here -->
-      </section>
+
     </section>
   </section>
 
