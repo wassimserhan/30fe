9d2510ac459fa62520e59c28ec01f336cc766991
Wassim Serhan <wserhan@gmail.com>
2023-09-05 10:55:58 -0400

    edit

--- wp-content/themes/30fe/src/modules/search.js
+++ wp-content/themes/30fe/src/modules/search.js
@@ -115,7 +115,7 @@ class Search {
                 <a class="search__result__link" href="${item.permalink}">
                   <section class="search__result">
 
-                 ${item.image ? `<img src="${item.image}">` : `<img src="${siteData.root_url}/wp-content/themes/30fe/images/blog/general.png">`}
+                 ${item.image ? `<img src="${item.image}">` : `<img src="${siteData.root_url}/wp-content/themes/30fe/images/blog/General.png">`}
 
 
                 
