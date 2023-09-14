0d1c28da96c9e832c44d9c062045891eeed28e47
Wassim Serhan <wserhan@gmail.com>
2023-09-14 14:38:58 -0400

    edit

--- wp-content/themes/30fe/single-team.php
+++ wp-content/themes/30fe/single-team.php
@@ -70,7 +70,7 @@ if( $terms ): ?>
           <h6 class="member__competencies">Specialized Expertise</h6>
           <hr class="member__line">
           <?php foreach( $terms as $term ): ?>
-
+          <!-- pill -->
           <section class="pill">
             <p class="pill__label"><?php echo $term->name; ?></p>
             <?php endforeach; ?>
