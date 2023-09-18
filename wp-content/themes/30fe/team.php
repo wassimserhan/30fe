fd258a373dde7fe560caf1d0caacfd81bcdd0bc0
Wassim Serhan <wserhan@gmail.com>
2023-09-18 10:32:34 -0400

    edit

--- wp-content/themes/30fe/team.php
+++ wp-content/themes/30fe/team.php
@@ -51,15 +51,15 @@ get_header();
         <h3 class="team__roles">Leadership Team</h3>
         <section id="team" class="team__grid">
           <?php 
-          $team_members = get_field ( 'team_member' ); 
-          foreach ($team_members as $team_member):     
-          $name = get_the_title($team_member);
-          $title = get_field('title', $team_member);
+          $leadership = get_field ( 'leadership' ); 
+          foreach ($leadership as $item):     
+          $name = get_the_title($item);
+          $title = get_field('title', $item);
 
         ?>
           <article class="team__card">
-            <a href="<?php the_permalink($team_member) ?>">
-              <img class="lazyload team__card__image" src="<?php echo get_the_post_thumbnail_url($team_member); ?>"
+            <a href="<?php the_permalink($item) ?>">
+              <img class="lazyload team__card__image" src="<?php echo get_the_post_thumbnail_url($item); ?>"
                 alt="<?php echo $name; ?>" title="<?php echo $name; ?>">
 
             </a>
@@ -70,7 +70,7 @@ get_header();
               <?php echo $title ?>
             </p>
             <p class="team__card__expertise" style="display: none">
-              <?php $expertise = get_field('expertise', $team_member);
+              <?php $expertise = get_field('expertise', $item);
             foreach ($expertise as $item):?>
               <?php echo get_the_title($item);  ?>
               <?php endforeach; ?></p>
@@ -84,14 +84,14 @@ get_header();
         <section id="team" class="team__grid">
           <?php 
           $experts = get_field ( 'experts' ); 
-          foreach ($experts as $expert):     
-          $name = get_the_title($expert);
-          $title = get_field('title', $expert);
+          foreach ($experts as $item):     
+          $name = get_the_title($item);
+          $title = get_field('title', $item);
 
         ?>
           <article class="team__card">
-            <a href="<?php the_permalink($expert) ?>">
-              <img class="lazyload team__card__image" src="<?php echo get_the_post_thumbnail_url($expert); ?>"
+            <a href="<?php the_permalink($item) ?>">
+              <img class="lazyload team__card__image" src="<?php echo get_the_post_thumbnail_url($item); ?>"
                 alt="<?php echo $name; ?>" title="<?php echo $name; ?>">
 
             </a>
@@ -102,7 +102,7 @@ get_header();
               <?php echo $title ?>
             </p>
             <p class="team__card__expertise" style="display: none">
-              <?php $expertise = get_field('expertise', $expert);
+              <?php $expertise = get_field('expertise', $item);
             foreach ($expertise as $item):?>
               <?php echo get_the_title($item);  ?>
               <?php endforeach; ?></p>
