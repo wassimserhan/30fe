c0cae97747e612e5db3d27f8aad0c2a8e338b9af
Wassim Serhan <wassim@Mac.lan>
2024-12-10 11:10:52 -0500

    forms

--- wp-content/themes/30fe/submit.php
+++ wp-content/themes/30fe/submit.php
@@ -5,16 +5,16 @@ get_header();
 ?>
 
 <main class="main-container">
-  <section class="email fullscreen">
-    <section class="max-width max-padding">
-      <h2 class="email__headline"><?php echo get_field('title') ?></h2>
-      <p class="email__copy"><?php echo get_field('subheading') ?></p>
-      <section class="email__form">
-        <?php echo get_field('form_code') ;?>
-      </section>
+    <section id="submit-assign" class="email fullscreen">
+        <section class="max-width max-padding">
+            <h2 class="email__headline"><?php echo get_field('title') ?></h2>
+            <p class="email__copy"><?php echo get_field('subheading') ?></p>
+            <section class="email__form">
+                <?php echo get_field('form_code') ;?>
+            </section>
 
+        </section>
     </section>
-  </section>
 
 </main>
 
