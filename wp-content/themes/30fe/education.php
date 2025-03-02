badb8fc870f84e9c935763ceddfe1c0e1a8365b4
Wassim Serhan <wserhan@gmail.com>
2025-03-02 08:52:02 -0500

    font

--- wp-content/themes/30fe/education.php
+++ wp-content/themes/30fe/education.php
@@ -10,12 +10,11 @@ get_header();
                 <section class="max-width max-padding">
                     <article>
                         <h2 class="email__headline">Continuing Education</h2>
-                        <p class="email__copy">We offer continuing education courses, seminars and webinars for
-                            insurance and legal professionals. Our experts are certified by the California State Bar and
-                            our courses are accredited by the Law Society of Ontario and the Law Society of BC. Seminars
-                            can be combined or customized as requested. To schedule a complimentary session for any of
-                            the topics listed below, or to book a custom seminar, please reach out to our Vancouver,
-                            Toronto or LA contact.
+                        <p class="email__copy">Welcome to 30 Forensic Engineering’s continuing education—your gateway to
+                            expert-led professional development. Our complimentary, accredited courses cater to
+                            professionals in the insurance and legal sectors. With a robust offering of over 40 courses,
+                            we provide flexible learning formats, including webinars, in-person sessions, and customized
+                            content to meet your team's unique needs. Please explore our curriculum below:
                         </p> <a href="<?php the_permalink() ?>" aria-label="Read more">
                             <button class="education__button">Request a Course</button>
                         </a>
@@ -29,7 +28,6 @@ get_header();
           if( have_rows('column_list') ): 
           while( have_rows('column_list') ) : the_row();
             $educationItems = get_sub_field('list_item'); ?>
-
             <div class="ac">
                 <div class="ac-header mobile-accordion__items mobile-accordion__items--education max-padding--side">
                     <p class="mobile-accordion__number education__number">
@@ -38,42 +36,10 @@ get_header();
                 </div>
                 <div class="ac-panel max-padding--side education__bullets">
                     <section class="industry__category-bullets education__category-bullets">
-                        <?php
-                if( have_rows('items') ): 
-                while( have_rows('items') ) : the_row();
-                  $educationItem = get_sub_field('item'); ?>
-                        <div class="industry__category-bullets__items">
-                            <figure class="industry__category-bullets__arrow education__arrow">
-                                <img loading="lazy"
-                                    src="<?php echo get_template_directory_uri(); ?>/images/education-arrow.svg"
-                                    width="20" height="20" alt="<?php echo $alt; ?>" title="<?php echo $title; ?>">
-                            </figure>
-                            <article>
-                                <p class="ac-text industry__category-bullets__item"><?php echo $educationItem ?></p>
-
-
-                                <?php
-                if( have_rows('subitems') ): ?>
-                                <ul class="industry__category-bullets__subitem">
-                                    <?php  
-                      while( have_rows('subitems') ) : the_row();
-                       $subItem = get_sub_field('subitem'); ?>
-                                    <li><?php echo $subItem ?></li>
-                                    <?php
-                      endwhile; ?>
-                                </ul>
-                                <?php endif;?>
-                            </article>
-                        </div>
-                        <?php
-                    endwhile; ?>
-                        <?php endif;?>
+                        <?php echo get_sub_field('copy') ?>
                     </section>
                 </div>
-
-
             </div>
-
             <?php
             endwhile; ?>
             <?php endif;
